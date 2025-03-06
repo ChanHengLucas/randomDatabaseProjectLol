bc3afec77eafa77bff6671ae1df56853aa2971ce
ChanHengLucas <91720922+ChanHengLucas@users.noreply.github.com>
2025-03-06 12:07:48 -0500

    o noes an error i so scaregit add *! :(

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -6,6 +6,41 @@ import { PokemonService } from './pokemon.service';
 export class PokemonController {
     constructor(private readonly pokemonService: PokemonService) {}
 
-    // @Post()
+    @Post()
+    async addPokemon(
+        @Body('name') pokemonName: string,
+        @Body('type1') pokemonType1: string,
+        @Body('type2') pokemonType2: string,
+        @Body('height') pokemonHeight: number,
+        @Body('weight') pokemonWeight: number,
+        @Body('dexNumber') pokemonDexNumber: number,
+        @Body('image') pokemonImage: string,
+        @Body('shinyImage') pokemonShinyImage: string,
+        @Body('cry') pokemonCry: string,
+    ) {
+        const generatedId = await this.pokemonService.insertPokemon(
+            pokemonName,
+            pokemonType1,
+            pokemonType2,
+            pokemonHeight,
+            pokemonWeight,
+            pokemonDexNumber,
+            pokemonImage,
+            pokemonShinyImage,
+            pokemonCry,
+        );
+        return { id: generatedId };
+    }
+
+    @Get()
+    async getAllPokemon() { // get ALL Pokemon
+        const pokemons = await this.pokemonService.getPokemon();
+        return pokemons;
+    }
+
+    @Get(':id')
+    getPokemon(@Param('id') pokemonId: string) {
+        return this.pokemonService.getSinglePokemon(pokemonId);
+    }
 
 }
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -8,4 +8,74 @@ import { Pokemon } from './pokemon.model';
 export class PokemonService {
     constructor(@InjectModel('Pokemon') private readonly pokemonModel: Model<Pokemon>) {}
 
+    async insertPokemon(
+        name: string,
+        type1: string,
+        type2: string,
+        height: number,
+        weight: number,
+        dexNumber: number,
+        image: string,
+        shinyImage: string,
+        cry: string,
+    ) {
+        const newPokemon = new this.pokemonModel({
+            name,
+            type1,
+            type2,
+            height,
+            weight,
+            dexNumber,
+            image,
+            shinyImage,
+            cry,
+        });
+        const result = await newPokemon.save(); // waits for the save to finish
+        return result.id as string;
+    }
+
+    async getPokemon() {
+        const pokemons = await this.pokemonModel.find().exec(); // returns a promise
+        return pokemons.map(pokemon => ({
+            id: pokemon.id,
+            name: pokemon.name,
+            type1: pokemon.type1,
+            type2: pokemon.type2,
+            height: pokemon.height,
+            weight: pokemon.weight,
+            dexNumber: pokemon.dexNumber,
+            image: pokemon.image,
+            shinyImage: pokemon.shinyImage,
+            cry: pokemon.cry,
+        })); // yields a list of Pokemon
+    }
+
+    async getSinglePokemon(pokemonId: string){
+        const pokemon = await this.findPokemon(pokemonId);
+        return {
+            id: pokemon.id,
+            name: pokemon.name,
+            type1: pokemon.type1,
+            type2: pokemon.type2,
+            height: pokemon.height,
+            weight: pokemon.weight,
+            dexNumber: pokemon.dexNumber,
+            image: pokemon.image,
+            shinyImage: pokemon.shinyImage,
+            cry: pokemon.cry,
+        };
+    }
+
+    private async findPokemon(id: string) {
+        let pokemon;
+        try {
+            pokemon = await this.PokemonService.getSinglePokemon(id);
+        } catch (error) {
+            throw new Error('Could not find Pokemon.');
+        }
+        if (!pokemon) {
+            throw new Error('Could not find Pokemon.');
+        }
+        return pokemon;
+    }
 }
